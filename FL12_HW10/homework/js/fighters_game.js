829bca87b4080c78ec544fbfbdcec92e407d4438
Ihor Pedyna <58253728+ihorpedyna5@users.noreply.github.com>
2020-01-20 22:05:46 +0200

    Add files via upload

---
+++ FL12_HW10/homework/js/fighters_game.js
@@ -0,0 +1,72 @@
+function Fighter (object){
+			const name= object.name, damage = object.damage, strength = object.strength,
+			agility = object.agility, hpTotal = object.hp;
+			let hpCurrent = object.hp, wins = 0, losses = 0;
+			this.getName = function () {
+			return name;
+}
+
+this.getDamage = function (){
+	return damage;
+}
+this.getStrength = function (){
+	return strength;
+}
+this.getAgility = function (){
+	return agility;
+}
+this.getHealth = function (){
+	return hpCurrent;
+}
+
+this.attack = function (enemy){
+			const PERCENT = 100;
+			if (Math.ceil(Math.random() * PERCENT) < 
+			PERCENT - (enemy.getStrength() + enemy.getAgility())){
+			enemy.dealDamage(damage);
+			console.log(`${name} makes ${damage} damage to ${enemy.getName()}`);
+			} else{
+			console.log(`${name} attack missed`);
+			}
+}
+
+this.logCombatHistory = function (){
+		console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`);
+}
+
+this.heal = function (addHealth){
+		hpCurrent = hpCurrent + addHealth > hpTotal ? hpTotal : hpCurrent + addHealth;
+}
+
+this.dealDamage = function (damegeFromEnemy){
+		const MIN_HP = 0;
+		hpCurrent = hpCurrent - damegeFromEnemy < MIN_HP ? MIN_HP : hpCurrent - damegeFromEnemy;
+}
+
+this.addWin = function (){
+		wins++;
+}
+
+this.addLoss = function (){
+		losses++;
+}	
+}
+
+function battle (first, second){
+		let odd = Math.round(Math.random()), fighter, defender;
+		if (!first.getHealth()){
+		console.log(`${first.getName()} is dead and can't fight.`);
+		} else if (!second.getHealth()){
+		console.log(`${second.getName()} is dead and can't fight.`);
+		} else {
+		do {
+			odd = !odd;
+			fighter = odd ? first : second;
+			defender = odd ? second : first;
+			fighter.attack(defender);
+		} while(defender.getHealth())
+		console.log(`${fighter.getName()} has won!`);
+		fighter.addWin();
+		defender.addLoss();
+	}
+}
